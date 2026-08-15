7fc8c0781faf4f3c12dbba4db625601d29542d40
paidophiloo <zgabriel.machado@gmail.com>
2026-08-15 19:18:03 -0300

    feat(newsroom): define player performance event types

---
+++ src/career-core/newsroom-performance-types.js
@@ -0,0 +1,19 @@
+export const NEWSROOM_PERFORMANCE_EVENT_TYPES = Object.freeze({
+  PLAYER_PERFORMANCE: 'player.performance',
+  PLAYER_MILESTONE: 'player.milestone'
+});
+
+export const PERFORMANCE_KINDS = Object.freeze({
+  FOUR_PLUS_GOALS: 'four-plus-goals',
+  HAT_TRICK: 'hat-trick',
+  BRACE: 'brace',
+  ASSIST_DOUBLE: 'assist-double',
+  STARTER_SHUTOUT: 'starter-shutout'
+});
+
+export const MILESTONE_KINDS = Object.freeze({
+  FIRST_CLUB_GOAL: 'first-club-goal',
+  SEASON_GOALS: 'season-goals'
+});
+
+export const SEASON_GOAL_MILESTONES = Object.freeze([5, 10, 15, 20, 25, 30, 35, 40, 50]);
