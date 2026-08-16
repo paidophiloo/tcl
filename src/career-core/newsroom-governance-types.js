8406c0ff74cb2860fe254f6e8153ec75e4bbf51a
paidophiloo <zgabriel.machado@gmail.com>
2026-08-16 12:10:39 -0300

    feat(newsroom): define governance and contract event types

---
+++ src/career-core/newsroom-governance-types.js
@@ -0,0 +1,20 @@
+export const NEWSROOM_GOVERNANCE_EVENT_TYPES = Object.freeze({
+  MANAGER_SACKED: 'manager.sacked',
+  MANAGER_HIRED: 'manager.hired',
+  MANAGER_POACHED: 'manager.poached',
+  CONTRACT_RENEWAL_REJECTED: 'contract.renewal-rejected',
+  CONTRACT_EXPIRED: 'contract.expired',
+  BOSMAN_PRECONTRACT_AGREED: 'contract.bosman-precontract-agreed',
+  BOSMAN_MOVE_COMPLETED: 'contract.bosman-move-completed'
+});
+
+export const GOVERNANCE_WORLD_TYPES = Object.freeze([
+  'MANAGER_SACKED',
+  'MANAGER_HIRED',
+  'MANAGER_POACHED',
+  'CONTRACT_RENEWED',
+  'CONTRACT_RENEWAL_REJECTED',
+  'CONTRACT_EXPIRED',
+  'BOSMAN_PRECONTRACT_AGREED',
+  'BOSMAN_MOVE_COMPLETED'
+]);
